4ffa512e801204715440222cd82c40eee4d02db7
Sebohbi <sebastian.ohlsson@honeybi.se>
2021-03-22 16:48:43 +0100

    Expanded exerciseapp with login session and a random picture

--- src/test/core.clj
+++ src/test/core.clj
@@ -12,7 +12,7 @@
 (def ds (jdbc/get-datasource db))                           ;Set ds as our db
 
 (def sessions (atom {}))
-;; exempel på hur den ser ut när någon är inloggad:
+;; exexmpel på hur den ser ut när någon är inloggad:
 ;;@sessions                                                   ;;Ger oss :val{}
 ;;=> {"jona" {:session-start #inst "2021-03-30 12:00:00"}}
 
@@ -40,7 +40,64 @@
 
   ;;Ha som mål att göra något på hemsidan så jag kan förändra sessions -> ska se i repl att sessions har ändrats
   ;;det som fylls i i inputfält ska hamna i sessions
+
+  (assoc-in {} [1 :connections 4] 2)
+  (def username "Knauf")
+
+  (def users (assoc-in {} [username :session-start] (java.util.Date.)))
+
+  users
+
+  (assoc-in )
+
+  (def usersession (atom {}))
+  (reset! usersession {})
+
+  (swap! usersession (fn [ss] (assoc-in [0] ss :username "Hello")
+                       ))
+  @usersession
   )
+
+(comment
+  " Nästa steg: Skapa användartabell (USER) med pk och användarnamn
+  pk är primarykey unikt id har inget - tänk sqlnummret - tänkpersonnummer, du kan byta namn och annat men ej nummer
+  pk kan ha auto-increment
+  användarnamn kan vara varchar [50]
+
+
+  med tabell definerar vi en databastabell
+
+  Sen vill vi ha:
+  en permissionstabell -ska heta  USER_ROLE
+  Ska ha 2 fält, user-pk referens till användaren och ett fält för role  -varchar [50] tex admin eller rektor
+    user-pk ska vara en "foreignkey" med samma typ som pk
+
+
+  alla kolumnnamn ska vara UPPERCASE
+
+  tricky med permissions - grova eller granulära permissions. Det blir alltid fel  : D
+
+
+
+
+  Nästa steg:
+  funktioner vi behöveR:
+
+  1. Funktion för att Lägga till användare
+  2. Funktion för att Lägga till roller på användare  (ska kunna ta bort)
+  3. Funktion som tar ett Username och en ROLE och ger (has-role?) boolean beroende på access
+  4. -----"
+  )
+
+
+(defn add-user
+  "takes req and adds user in the USER table"
+  [req]
+  (jdbc/execute! ds ["
+  insert into USER(USERNAME)
+    values(?)" todostring])                                 ;;HÄR SKA VI ERSÄTTA TODOSTRING MED VÄRDET VI VILL ERSÄTTA
+  )
+
 
 
 (defn row->li
@@ -75,53 +132,68 @@
   insert into todo(task,taskcomplete)
     values(?,?)" todostring false]))
 
+(defn mainlisthtml [req]
+  (html
+    [:h2 "This is a header for the list!"]
+    (vec (concat [:ul] (printtohtmlLI)))                    ;;Concatenates and vectorizes the vector printtohtmlLI with a vector containing unordered list to get html formatting
+    [:h2 "You can add notes below!"]
+    [:form
+     {:method  "post"
+      :action  "/postoffice"
+      :enctype "multipart/form-data"}                       ;;Change encoding to multipart/form-data
+     [:label {:for "#input1"} "Add new notes to the list:"]
+     [:input
+      {:type "text"
+       :id   "input1"
+       :name "input1"}]
+     [:input
+      {:type  "submit"
+       :value "Save"}]]))
+
+(defn sessionhtml [req]
+
+  (html
+    [:h2 "Save a session below!"]
+    [:form
+     {:method  "post"
+      :action  "/sessionoffice"
+      :enctype "multipart/form-data"}                       ;;Change encoding to multipart/form-data
+     [:label {:for "#input2"} "Type your username:"]
+     [:input
+      {:type "text"
+       :id   "input2"
+       :name "input2"}]
+     [:input
+      {:type  "submit"
+       :value "Save"}]]
+    " Atom sessions values:"
+    @sessions [:br]
+    "You are logged in as: "
+    (:username @sessions) [:br]
+    "Current time and date is: "
+    (:timeanddate @sessions) [:br]))
+
+
+(defn imagetohtml [req]
+  (html
+    [:h2 "This is a copyright free picture of a random person riding an escalator in a wierd format!"]
+    [:img {:src "https://cdn.stocksnap.io/img-thumbs/960w/busy-businessman_UTCYYGKHZT.jpg", :alt "It's a person?", :width "350", :height "200", :style "border:3px solid black"}])
+  )
+
 (defn main-handler [req]
-  (def tempsession @sessions)
   (html
     [:div
      [:h1 "This is a big header!"]
-     [:h2 "This is a header for the list!"]
-     (vec (concat [:ul] (printtohtmlLI)))                   ;;Concatenates and vectorizes the vector printtohtmlLI with a vector containing unordered list to get html formatting
-     [:h2 "You can add notes below!"]
-     [:form
-      {:method  "post"
-       :action  "/postoffice"
-       :enctype "multipart/form-data"}                      ;;Change encoding to multipart/form-data
-      [:label {:for "#input1"} "Add new notes to the list:"]
-      [:input
-       {:type "text"
-        :id   "input1"
-        :name "input1"}]
-      [:input
-       {:type  "submit"
-        :value "Save"}]]
-     [:h2 "Save a session below!"]
-     [:form
-      {:method  "post"
-       :action  "/sessionoffice"
-       :enctype "multipart/form-data"}                      ;;Change encoding to multipart/form-data
-      [:label {:for "#input2"} "Type your username:"]
-      [:input
-       {:type "text"
-        :id   "input2"
-        :name "input2"}]
-      [:input
-       {:type  "submit"
-        :value "Save"}]]
-     " Atom sessions values:"
-     @sessions [:br]
-     "You are logged in as: "
-     (:username tempsession) [:br]
-     "Current time and date is: "
-     (:timeanddate tempsession) [:br]
-     [:h2 "This is a copyright free picture of a random person riding an escalator"]
-     [:img {:src "https://cdn.stocksnap.io/img-thumbs/960w/busy-businessman_UTCYYGKHZT.jpg", :alt "It's a person?"}]
-     ]))
+     (mainlisthtml req)
+     (sessionhtml req)
+     (imagetohtml req)]))
+
+
 
 (defn session-handler [req]
   ;(println (get-in req [:params "input2"]))
-  (swap! sessions (fn [ss] (assoc ss :username (get-in req [:params "input2"])
-                                     :timeanddate (java.util.Date.))))
+  (swap! sessions (fn [ss] (assoc ss (get-in req [:params "input2"])
+                                     {:timeanddate (java.util.Date.)})))
   (main-handler req))
 
 (defn mail-handler [req]
@@ -159,14 +231,59 @@
 (comment
   (-main)                                                   ;;Boot server here
 
-
-
   ;;Creates the todo-table, (already created)
   (jdbc/execute! ds ["
   create table todo (
   id int auto_increment primary key,
   task varchar(255),
   taskcomplete boolean)"])
+
+  ;;Creates the userdata-tabla
+  (jdbc/execute! ds ["
+  create table USER (
+  PK int auto_increment primary key,
+  USERNAME varchar(50))"])
+
+  (jdbc/execute! ds ["
+  create table USER_ROLE (
+  USER_PK int,
+  ROLE varchar(50),
+    FOREIGN KEY (USER_PK)
+    REFERENCES USER (PK))"])
+
+
+  ;;;SYNTAX FÖR POSTRESQL <- använd sen!
+  ;;Creates the permissiontable
+  (jdbc/execute! ds ["
+  create table USER_ROLE (
+  USER_PK int,
+  ROLE varchar(50),
+  CONSTRAINT FK_USER
+    FOREIGN KEY(USER_PK)
+    REFERENCES USER(PK))"])
+
+"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  Nästa steg: Skapa användartabell (USER) med pk och användarnamn\n  pk är primarykey unikt id har inget - tänk sqlnummret - tänkpersonnummer, du kan byta namn och annat men ej nummer\n  pk kan ha auto-increment\n  användarnamn kan vara varchar [50]\n\n\n  med tabell definerar vi en databastabell\n\n  Sen vill vi ha:\n  en permissionstabell -ska heta  USER_ROLE\n  Ska ha 2 fält, user-pk referens till användaren och ett fält för role  -varchar [50] tex admin eller rektor\n  user-pk ska vara en \"foreignkey\" med samma typ som pk\n\n\n  alla kolumnnamn ska vara UPPERCASE\n\n  tricky med permissions - grova eller granulära permissions. Det blir alltid fel  : D
+
+
+
+  "
+
 
   ;;Inserts a demo-value in the table, (already in the table)
   (jdbc/execute! ds ["
